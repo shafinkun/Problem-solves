64425db6ee0a6f6dadbb24fc6c09b6c8b345d084
Shazid Mashrafi <shazid456@gmail.com>
2023-02-19 23:34:08 +0600

    initial commit

---
+++ Beecrowd/1013.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,max;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>b && a>c) max=a;
+    if(b>a && b>c) max=b;
+    if(c>a && c>b) max=c;
+    printf("%d eh oh maior\n",max);
+    return 0;
+}
