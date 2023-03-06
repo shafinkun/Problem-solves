b0dcbf0c76034be35ef400c8caa420b98e327586
ShazidMashrafi <shazid456@gmail.com>
2023-03-06 15:24:47 +0600

    update logic

--- Tushar sir problems/problem3.c
+++ Tushar sir problems/problem3.c
@@ -5,6 +5,8 @@ int main()
     int num,temp,r,sum=0;
     printf("Enter your three digit number: ");
     scanf("%d",&num);
+    if(num>99 && num<1000)
+    {
     temp=num;
     while(temp!=0)
     {
@@ -14,5 +16,6 @@ int main()
     }
     if(num==sum) printf("The number is a palindrome number");
     else printf("The number is not a palindrome number");
-    return 0;
+    }
+    else printf("Wrong input.\n");
 }
