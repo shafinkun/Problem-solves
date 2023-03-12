0ba33347d7e2cd0309e374b496b9b15b06ab2ae5
ShazidMashrafi <shazid456@gmail.com>
2023-03-12 22:53:35 +0600

    initial commit

--- Beecrowd/2757.c
+++ Beecrowd/2757.c
@@ -3,8 +3,8 @@ int main()
 {
     int a,b,c;
     scanf("%d %d %d",&a,&b,&c);
-    printf("A = %d, B = %d, C= %d\n",a,b,c);
-    printf("A = %10d, B = %10d, C= %10d\n",a,b,c);
+    printf("A = %d, B = %d, C = %d\n",a,b,c);
+    printf("A = %10d, B = %10d, C = %10d\n",a,b,c);
     printf("A = %010d, B = %010d, C = %010d\n",a,b,c);
     printf("A = %-10d, B = %-10d, C = %-10d\n",a,b,c);
 }
