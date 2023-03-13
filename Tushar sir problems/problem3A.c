09c39bf863bcc26315c1482e85a9f9e1a50f63ef
ShazidMashrafi <shazid456@gmail.com>
2023-03-13 23:59:21 +0600

    removed lib function and did it manually

---
+++ Tushar sir problems/problem3A.c
@@ -0,0 +1,26 @@
+// Palindrome check using array
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i,j,n,len=0,flag=0;
+    int arr[20];
+    printf("Enter your number: ");
+    scanf("%d",&n);
+    for(i=0;n>0;i++)
+    {
+        arr[i]=n%10;
+        n/=10;
+        len++;
+    }
+    for(i=0;i<len/2;i++)
+    {
+        if(arr[i]!=arr[len-1-i])
+        {
+            flag++;
+            break;
+        }
+    }
+    if(flag==0) printf("The number is palindrome.\n");
+    else printf("The number is not palindrome.\n");
+}
