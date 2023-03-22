9ed4b61126bd29e8dcc15646008a15e747c1bbfd
ShazidMashrafi <shazid456@gmail.com>
2023-03-22 15:29:44 +0600

    initial commit

---
+++ Tushar sir problems/problem5A.c
@@ -0,0 +1,23 @@
+//Prime numbers up to N
+#include<stdio.h>
+int main()
+{
+    int n,flag,i,j;
+    printf("Enter N: ");
+    scanf("%d",&n);
+    for(i=2;i<=n;i++)
+    {
+        flag=0;
+        for(j=2;j<i;j++)
+        {
+            if(i%j==0)
+            {    
+                flag=1;
+            }
+        }
+        if(flag==0)
+            {
+                printf("%d ",i);
+            } 
+    }
+}
