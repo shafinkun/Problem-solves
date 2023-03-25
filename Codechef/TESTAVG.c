c514368db0da3e5da059c1131f816e6ebbb25375
ShazidMashrafi <shazid456@gmail.com>
2023-03-26 00:03:04 +0600

    upate logic

--- Codechef/TESTAVG.c
+++ Codechef/TESTAVG.c
@@ -6,9 +6,7 @@ int main()
     while(t--)
     {
         scanf("%d %d %d",&x,&y,&z);
-        if((x+y)/2<35) printf("Fail\n");
-        else if((y+z)/2<35) printf("Fail\n");
-        else if((x+z)/2<35) printf("Fail\n");
-        else printf("Pass\n");
+        if((x+y)/2>=35 && (y+z)/2>=35 && (z+x)/2>=35) printf("Pass\n");
+        else printf("Fail\n");
     }
 }
