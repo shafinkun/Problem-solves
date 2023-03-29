ad85c1b222ce80a74dc49f8b05e7971583988efe
ShazidMashrafi <shazid456@gmail.com>
2023-03-29 12:18:16 +0600

    initial commit

---
+++ Tushar sir problems/Gradepoint_if_else.c
@@ -0,0 +1,21 @@
+//Grading point for Bangladesh universities using if else
+
+#include<stdio.h>
+int main()
+{
+    int marks;
+    float grade;
+    printf("Enter the marks of the student: ");
+    scanf("%d",&marks);
+    if(marks>=80) grade=4.0;
+    else if(marks>=75) grade=3.75;
+    else if(marks>=70) grade=3.50;
+    else if(marks>=65) grade=3.25;
+    else if(marks>=60) grade=3.00;
+    else if(marks>=55) grade=2.75;
+    else if(marks>=50) grade=2.50;
+    else if(marks>=45) grade=2.25;
+    else if(marks>=40) grade=2.00;
+    else grade=0.00;
+    printf("The obtained grade is %.2f",grade);
+}
---
+++ Tushar sir problems/Gradepoint_switch.c
@@ -0,0 +1,46 @@
+//Grading point of Bangladeshi universities using switch
+
+#include<stdio.h>
+int main()
+{
+    int marks,m,r;
+    float grade;
+    printf("Enter the marks of the student: ");
+    scanf("%d",&marks);
+    m=marks/10;
+    r=marks%10;
+    switch(m)
+    {
+        case 10:
+        case 9:
+        case 8:
+                grade=4;
+                break;
+        case 7:
+            if(r>=5)
+                grade=3.75;
+            else grade=3.50;
+            break;
+        case 6:
+            if(r>=5)
+                grade=3.25;
+            else grade=3.00;
+            break;
+        case 5:
+            if(r>=5)
+                grade=2.75;
+            else grade=2.50;
+            break;
+        case 4:
+            if(r>=5)
+                grade=2.25;
+            else grade=2.00;
+            break;
+        case 3:
+        case 2:
+        case 1:
+        case 0:
+            grade=0.00;
+    }
+    printf("Obtained grade is %.2f",grade);
+}
