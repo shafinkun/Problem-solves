ef8fecf4f0377c388b9de82226921c6ce298c3ef
ShazidMashrafi <shazid456@gmail.com>
2023-04-05 14:04:01 +0600

    update logic

--- Tushar sir problems/problem3.c
+++ Tushar sir problems/problem3.c
@@ -8,11 +8,11 @@ int main()
     if(num>99 && num<1000)
     {
     temp=num;
-    while(temp!=0)
+    while(temp>0)
     {
         r=temp%10;
         sum=sum*10+r;
-        temp=temp/10;
+        temp=temp/10; 
     }
     if(num==sum) printf("The number is a palindrome number");
     else printf("The number is not a palindrome number");
